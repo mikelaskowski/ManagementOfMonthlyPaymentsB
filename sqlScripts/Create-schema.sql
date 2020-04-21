b670312765a32c73f5ce28156de614e0988d224d
Michal Laskowski <mikelaskowski@o2.pl>
2020-04-21 16:12:29 +0200

    create testing servlet wchich test connection with Db MySQL

---
+++ sqlScripts/Create-schema.sql
@@ -0,0 +1,2 @@
+CREATE SCHEMA  IF NOT EXISTS `management_of_monthly_payments` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci ;
+USE `management_of_monthly_payments`;
---
+++ sqlScripts/Create-user.sql
@@ -0,0 +1,15 @@
+CREATE USER 'koronawirus2020'@'localhost' IDENTIFIED BY 'koronawirus2020';
+
+GRANT ALL PRIVILEGES ON * . * TO 'koronawirus2020'@'localhost';
+
+#
+# Starting with MySQL 8.0.4, the MySQL team changed the 
+# default authentication plugin for MySQL server 
+# from mysql_native_password to caching_sha2_password.
+#
+# The command below will make the appropriate updates for your user account.
+#
+# See the MySQL Reference Manual for details: 
+# https://dev.mysql.com/doc/refman/8.0/en/caching-sha2-pluggable-authentication.html
+#
+ALTER USER 'koronawirus2020'@'localhost' IDENTIFIED WITH mysql_native_password BY 'koronawirus2020';
