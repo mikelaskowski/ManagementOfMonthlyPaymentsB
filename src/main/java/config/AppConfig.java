943d7239b3b7db49cd9103ab1e1ac26e601ce319
Michal Laskowski <mikelaskowski@o2.pl>
2020-04-22 11:44:32 +0200

    create config Classess for Spring(connection using Hibernate with MySql db)

---
+++ src/main/java/config/AppConfig.java
@@ -0,0 +1,122 @@
+package config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+import java.util.logging.Logger;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(value = "com.mikelaskowski.MOMP")
+@PropertySource(value = {"classpath:persistence-mysql.properties"})
+public class AppConfig {
+
+    @Autowired
+    private Environment env;
+
+    private final Logger LOGGER = Logger.getLogger(getClass().getName());
+
+    // ============================= define a bean for DataBase ======================================================//
+
+    @Bean
+    public DataSource myDataSource() {
+
+        // create connection pool
+        ComboPooledDataSource myDataSource = new ComboPooledDataSource();
+
+        // set the jdbc driver
+
+        try {
+            myDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+
+        // log url and user ... to make sure we are reading the data
+        LOGGER.info("jdbc.url= " + env.getProperty("jdbc.url"));
+        LOGGER.info("jdbc.user= " + env.getProperty("jdbc.user"));
+
+        // set database connection properties
+        myDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        myDataSource.setUser(env.getProperty("jdbc.user"));
+        myDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        //set connection pool properties
+        myDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        myDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        myDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        myDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return myDataSource;
+    }
+
+    // need a helper method
+    // read environment property and convert to int
+
+    private int getIntProperty(String propName) {
+
+        String propVal = env.getProperty(propName);
+
+        // now convert to int
+        int intPropVal = Integer.parseInt(propVal);
+
+        return intPropVal;
+    }
+
+    // ============================= define a bean for Hibernate =====================================================//
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+
+        //create session factory
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        // set the properties
+        sessionFactory.setDataSource(myDataSource());
+        sessionFactory.setPackagesToScan(env.getProperty("hibernate.packagesToScan"));
+        sessionFactory.setHibernateProperties(getHibernateProperties());
+
+        return sessionFactory;
+    }
+
+    // need a helper method
+    // read environment property and convert to Properties object
+
+    private Properties getHibernateProperties() {
+
+        // set hibernate properties
+        Properties props = new Properties();
+
+        props.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+        props.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
+
+        return props;
+    }
+
+    @Bean
+    @Autowired
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+
+        // setup transaction manager based on session factory
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
+        txManager.setSessionFactory(sessionFactory);
+
+        return txManager;
+    }
+
+
+}
